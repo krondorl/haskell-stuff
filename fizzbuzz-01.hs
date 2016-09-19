6f6cacf2c712dd468d9342ab9d3bba68bc5f4429
Adam Burucs <adam.burucs@gmail.com>
2016-09-20 01:16:54 +0200

    create fizzbuzz01
    Simple fizzbuzz function

---
+++ fizzbuzz-01.hs
@@ -0,0 +1,8 @@
+fizzBuzz :: Integer -> String
+fizzBuzz n
+    | n `mod` 3 == 0 && n `mod` 5 == 0 = "fizzbuzz"
+    | n `mod` 3 == 0 = "fizz"
+    | n `mod` 5 == 0 = "buzz"
+    | otherwise      = "normal"
+
+main = putStrLn (fizzBuzz 15)
